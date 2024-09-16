6a775e7404b884dabd2327683a17517d78b18749
unknown <guilherme.psantos.dev@gmail.com>
2024-09-16 13:27:40 -0300

    DecodificadorDeTexto

---
+++ #Decodificador de Texto ONE (Alura)README.txt
@@ -0,0 +1,27 @@
+#Decodificador de Texto ONE (Alura)
+
+Este projeto foi desenvolvido como parte do curso "Praticando lógica de programação: Challenge Decodificador de Texto" da Alura. O objetivo do curso é aplicar e reforçar conceitos fundamentais de lógica de programação através da criação de um decodificador de texto.
+
+Funcionalidades Implementadas no projeto:
+
+Codificação e Decodificação de Texto: O projeto permite que o usuário converta texto de acordo com a regra específica, substituindo vogais por sequências de caracteres predefinidas.
+
+As "chaves" de criptografia que foram utilizadas são: A letra "e" é convertida para "enter" A letra "i" é convertida para "imes" A letra "a" é convertida para "ai" A letra "o" é convertida para "ober" A letra "u" é convertida para "ufat"
+Requisitos:
+
+Deve funcionar apenas com letras minúsculas;
+Não devem ser utilizados letras com acentos nem caracteres especiais;
+Deve ser possível converter uma palavra para a versão criptografada e também retornar uma palavra criptografada para a versão original; Por exemplo: "gato" => "gaitober" "gaitober" => "gato"
+Interação com o Usuário: -Interface simples onde o usuário pode inserir o texto, realizar a codificação ou decodificação, e ver o resultado diretamente na tela. Além de, um botão para copiar o texto;
+
+Conceitos Praticados:
+
+Manipulação de strings;
+Controle de fluxo (condicionais e loops);
+Funções em JavaScript;
+Manipulação do DOM com JavaScript;
+Este projeto serve:
+
+Como uma ferramenta de prática para reforçar conceitos básicos de lógica de programação;
+O decodificador de texto criado permite que o usuário experimente a codificação e decodificação de mensagens e visa que somente as pessoas que sabem da regra de encriptação consiga fazer a intrepretação do texto;
+Além de ser um exercício prático, o projeto também demonstra como aplicar esses conceitos em um cenário real, simulando uma aplicação que poderia ser expandida para casos de uso mais complexos;
---
+++ script.js
@@ -0,0 +1,68 @@
+const textArea = document.querySelector("#entradaTexto");
+const mensagem = document.querySelector("#mensagem"); 
+
+// As "chaves" de criptografia que utilizaremos são:
+// A letra "e" é convertida para "enter"
+// A letra "i" é convertida para "imes"
+// A letra "a" é convertida para "ai"
+// A letra "o" é convertida para "ober"
+// A letra "u" é convertida para "ufat"
+
+function btnEncriptar(){
+    const textoEncriptado = encriptar(textArea.value);
+    mensagem.value = textoEncriptado;
+    textArea.value = "";
+}
+
+function encriptar(stringEncriptada) {
+    document.getElementById('caixaDaMensagem').style.display = 'block';
+    document.getElementById('caixa-sem-nada').style.display = 'none';
+    let matrizCodigo = [["e" , "enter"] , ["i" , "imes"], ["a" ,"ai"] , ["o" , "ober"], ["u" , "ufat"] ];
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    for(let i = 0; i< matrizCodigo.length; i++){
+        if(stringEncriptada.includes(matrizCodigo[i][0])) {
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+        }
+    }
+    return stringEncriptada;
+}
+
+function btnDesencriptar(){
+    const textoDesencriptado = desencriptar(textArea.value);
+    mensagem.value = textoDesencriptado;
+    textArea.value = "";
+}
+
+function desencriptar(stringDesencriptada) {
+
+    let matrizCodigo = [["e" , "enter"] , ["i" , "imes"], ["a" ,"ai"] , ["o" , "ober"], ["u" , "ufat"] ];
+    stringDesencriptada = stringDesencriptada.toLowerCase();
+
+    for(let i = 0; i< matrizCodigo.length; i++){
+        if(stringDesencriptada.includes(matrizCodigo[i][1])) {
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
+        }
+    }
+    return stringDesencriptada;
+}
+
+function btnCopiar() {
+    navigator.clipboard.writeText(mensagem.value);
+    alert('Mensagem Copiada');
+}
+
+function removerAcentos(str) {
+    return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+}
+
+document.getElementById('texto').addEventListener('input', function () {
+
+    this.value = removerAcentos(this.value);
+});
+
+
+
+
+
+
